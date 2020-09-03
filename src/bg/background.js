1401a1618b8a2033d882a52d50821829eb43c4e8
Seungyeop Lee <sylee@teeware.kr>
2020-09-03 21:03:12 +0900

    Add test function to log current window count

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,29 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
+/*chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
-  });
+  });*/
+
+function getCurrentWindowCount() {
+	var getInfo = {
+		populate: true,
+		windowTypes: ['normal'],
+	};
+	var windowCount = 0;
+	return new Promise((resolve, reject) => {
+		chrome.windows.getAll(getInfo, windows => {
+			resolve(windows.length);
+		});
+	});
+}
+
+function logCurrentWindowCount() {
+	getCurrentWindowCount().then(count => {
+		console.log(count);
+	});
+}
+
+chrome.tabs.onCreated.addListener(logCurrentWindowCount);
